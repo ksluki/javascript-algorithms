b3c32e75b92a25e59df2b907cef310d2018020e3
Ksenia <ksluki98@yandex.ru>
2022-07-03 18:47:20 +0400

    1js

--- 05-find-vowels/script.js
+++ 05-find-vowels/script.js
@@ -4,17 +4,25 @@
  * Напишите функцию findVowels(str), принимающую на вход кириллическую
  * строку str  и возвращающую количество гласных, содержащихся в этой строке.
  * Для вашего удобства вот массив кириллических гласных:
- * 
+ *
  * ['а', 'я', 'о', 'ё', 'у', 'ю', 'ы', 'и', 'э', 'е'].
  *
-*/
+ */
 
 function findVowels(str) {
-    // Напишите код здесь
+  let vowels = ["а", "я", "о", "ё", "у", "ю", "ы", "и", "э", "е"];
+  let mas = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    str = str.toLowerCase();
+    if (vowels.includes(str[i])) {
+      mas += 1;
+    }
+  }
+  return mas;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(findVowels('здравствуй')); // 2
-console.log(findVowels('привет')); // 2
-console.log(findVowels('хеллоу')); // 3
+console.log(findVowels("здравствуй")); // 2
+console.log(findVowels("привет")); // 2
+console.log(findVowels("хеллоу")); // 3
